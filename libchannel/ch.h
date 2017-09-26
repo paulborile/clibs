8b47abd100ae146a5f0ceed95f8127c1671930b0
paulborile <paul.borile@gmail.com>
2017-09-26 13:48:02 +0200

    wrong prototype

--- libchannel/ch.h
+++ libchannel/ch.h
@@ -1,4 +1,31 @@
 
+/*
+   Copyright (c) 2003, Paul Stephen Borile
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+   1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+   2. Redistributions in binary form must reproduce the above copyright
+   notice
+   3. Neither the name of the Paul Stephen Borile nor the
+   names of its contributors may be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY Paul Stephen Borile ''AS IS'' AND ANY
+   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL Paul Stephen Borile BE LIABLE FOR ANY
+   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #ifndef CH_H
 #define CH_H
 
@@ -52,7 +79,7 @@ typedef struct _ch_h ch_h;
 
 // methods
 
-void *ch_create(int datalen);
+void *ch_create(ch_h *ch, int datalen);
 int ch_put(ch_h *ch, void *block);
 int ch_put_head(ch_h *ch, void *block);
 void *ch_get(ch_h *ch, void *block);
