05a588149143eae704dcb20651b58b66020c2cd3
paulborile <paul.borile@gmail.com>
2023-03-13 16:28:19 +0100

    missing makefile

---
+++ libthp/libthp.mk
@@ -0,0 +1,21 @@
+# LIB = library name
+# SRC = list of source files the library is made of.
+# RELEASE_CFLAGS = list of private Compiler flags for release build
+# DEBUG_CFLAGS = list of private Compiler flags for debug build
+# Available productions :
+#
+# release{linux,macosx,windows} : makes release version of the library
+# releaseclean
+# debug{linux,macosx,windows} : make debug version of the library with sanitize enabled
+# debugclean
+# coveragelinux : make coverage versione of library (lib<suffiz>-coverage.a)
+# coverageclean
+
+# library suffix
+LIB = thp
+SRC = thp.c
+# any personal cflag needed by the library code
+RELEASE_CFLAGS = -I../libchannel
+DEBUG_CFLAGS = -I../libchannel
+
+include	../makefiles/included_for_lib.mk
