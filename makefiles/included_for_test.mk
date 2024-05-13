c6e02f9eddedb86db0841177fdcded25c90e1b3d
paulborile <paul.borile@gmail.com>
2024-05-13 17:16:59 +0200

    also gtest should be compiled with c++14

--- makefiles/included_for_test.mk
+++ makefiles/included_for_test.mk
@@ -17,52 +17,52 @@ $(LOCAL_CXXFLAGS)
 CXXFLAGS_RELEASE = -std=c++11 -pthread -fpermissive -isystem ../../googletest/include -isystem ../../googletest \
 -isystem ../../picobench/include/ -O3 -I.. -I ../../libthp -I ../../libchannel -I ../../libtiming \
 $(LOCAL_CXXFLAGS)
-LDFLAGS = -pthread -L../ -L../../libthp -L ../../libchannel -L ../../libtiming -l$(LIB) $(LOCAL_LDFLAGS) -lpthread -lthp -lch -ltiming
+LDFLAGS = -pthread -L../ -L../../libthp -L ../../libchannel -L ../../libtiming -l$(LIB)-debug $(LOCAL_LDFLAGS) -lpthread -lthp -lch -ltiming
 
 # debug productions
 
 testlinux: $(OBJ)
 	(cd .. ; make -f lib$(LIB).mk debugclean ; make -f lib$(LIB).mk debuglinux)
-	$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
-	$(CXX) -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
+	-which infer && (cd .. ; make -f lib$(LIB).mk releaseclean; infer run -- make -f lib$(LIB).mk releaselinux)
+	$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+	$(CXX) -g3 -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
 
 testmacosx: $(OBJ)
 	(cd .. ; make -f lib$(LIB).mk debugclean ; make -f lib$(LIB).mk debuglinux)
-	$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
-	$(CXX) -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+	$(CXX) -g3 -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
 
 testwindows: $(OBJ)
 	(cd .. ; make -f lib$(LIB).mk debugclean ; make -f lib$(LIB).mk debuglinux)
-	$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
-	$(CXX) -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+	$(CXX) -g3 -o $(EXE) $(SANITIZE) $(OBJ) gtest-all.o $(LDFLAGS)
 
 testclean: indent
-		rm -rf $(OBJ) $(EXE) *.gcda *.gcno gtest-all.o ../../*_test_results.xml
+	rm -rf $(OBJ) $(EXE) *.gcda *.gcno gtest-all.o ../../*_test_results.xml
 
 testrun:
-			-./$(EXE) --gtest_output=xml:../../lib$(LIB)_test_results.xml $(RUN_OPTIONS)
+	-./$(EXE) --gtest_output=xml:../../lib$(LIB)_test_results.xml $(RUN_OPTIONS)
 
 
 # release compiled test productions
 
 rtestlinux: $(ROBJ)
 		(cd .. ; make -f lib$(LIB).mk releaseclean ; make -f lib$(LIB).mk releaselinux)
-		$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+		$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
 		$(CXX) -o r$(EXE) $(ROBJ) gtest-all.o $(LDFLAGS)
 
 rtestmacosx: $(ROBJ)
 		(cd .. ; make -f lib$(LIB).mk releaseclean ; make -f lib$(LIB).mk releaselinux)
-		$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+		$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
 		$(CXX) -o r$(EXE) $(ROBJ) gtest-all.o $(LDFLAGS)
 
 rtestwindows: $(ROBJ)
 		(cd ../ ; make -f lib$(LIB).mk releaseclean ; make -f lib$(LIB).mk releaselinux)
-		$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
+		$(CXX) $(CXXFLAGS) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
 		$(CXX) -o r$(EXE) $(ROBJ) gtest-all.o $(LDFLAGS)
 
 rtestclean: indent
 				rm -rf $(ROBJ) r$(EXE) *.gcda *.gcno gtest-all.o ../../*_test_results.xml
-
 rtestrun:
 	-./r$(EXE) --gtest_output="xml:../../lib$(LIB)_test_results.xml" $(RUN_OPTIONS)
 
@@ -73,7 +73,7 @@ indent:
 coveragelinux: $(OBJ)
 	(cd ../ ; make -f lib$(LIB).mk coverageclean ; make -f lib$(LIB).mk coveragelinux)
 	$(CXX) -isystem ../../googletest/include -I ../../googletest/ -pthread -c ../../googletest/src/gtest-all.cc
-	$(CXX) -o $(EXE) $(LOCAL_CXXFLAGS) $(SANITIZE) --coverage $(OBJ) gtest-all.o -L ../ -l$(LIB)-coverage $(LOCAL_LDFLAGS) \
+	$(CXX) -g3 -o $(EXE) $(LOCAL_CXXFLAGS) $(SANITIZE) --coverage $(OBJ) gtest-all.o -L ../ -l$(LIB)-coverage $(LOCAL_LDFLAGS) \
 	-lpthread -lthp -lch -ltiming
 	mkdir -p ../coverage
 	-./$(EXE) --gtest_output=xml:../../lib$(LIB)_test_results.xml $(RUN_OPTIONS)
